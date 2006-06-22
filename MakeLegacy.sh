a17308a7089860adac3cd2325d9eed6395617131
njh <njh@35dc7657-300d-0410-a2e5-dc2837fedb53>
2006-06-22 17:15:17 +0000

    I won my fight with sed! yay!
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@164 35dc7657-300d-0410-a2e5-dc2837fedb53

--- MakeLegacy.sh
+++ MakeLegacy.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 
-#LINE=`grep "AC_INIT(" configure.ac`
 #AC_INIT([mpg123], [0.60-devel], [mpg123-devel@lists.sourceforge.net])
-
-
+PACKAGE_NAME=`sed -n 's/^AC_INIT(\[\([^,]*\)\], .*$/\1/p' < configure.ac`
+PACKAGE_VERSION=`sed -n 's/^AC_INIT([^,]*, \[\([^,]*\)\], .*$/\1/p' < configure.ac`
+PACKAGE_BUGREPORT=`sed -n 's/^AC_INIT([^,]*, [^,]*, \[\(.*\)\])$/\1/p' < configure.ac`
 
 cd src
 
 # Need to extract this automatically from configure.ac
 echo "// Created by MakeLegacy.sh" > config.h
-echo "#define PACKAGE_NAME \"mpg123\"" >> config.h
-echo "#define PACKAGE_VERSION \"0.60-devel\"" >> config.h
-
+echo "#define PACKAGE_NAME \"$PACKAGE_NAME\"" >> config.h
+echo "#define PACKAGE_VERSION \"$PACKAGE_VERSION\"" >> config.h
+echo "#define PACKAGE_BUGREPORT \"$PACKAGE_BUGREPORT\"" >> config.h
 
 exec make -f Makefile.legacy $*
