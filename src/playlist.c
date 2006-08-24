931282f70561b72f5b2fdef4bdde20df1ffce86e
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2006-08-24 21:53:43 +0000

    boilerplates for playlist...
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@438 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/playlist.c
+++ src/playlist.c
@@ -1,3 +1,11 @@
+/*
+	playlist: playlist logic
+
+	copyright 1995-2006 by the mpg123 project - free software under the terms of the LGPL 2.1
+	see COPYING and AUTHORS files in distribution or http://mpg123.de
+	initially written by Michael Hipp, outsourced/reorganized by Thomas Orgis
+*/
+
 #include "config.h"
 #include "mpg123.h"
 #include "getlopt.h" /* for loptind */
--- src/playlist.h
+++ src/playlist.h
@@ -1,3 +1,11 @@
+/*
+	playlist: playlist logic
+
+	copyright 1995-2006 by the mpg123 project - free software under the terms of the LGPL 2.1
+	see COPYING and AUTHORS files in distribution or http://mpg123.de
+	initially written by Michael Hipp, outsourced/reorganized by Thomas Orgis
+*/
+
 void prepare_playlist(int argc, char** argv);
 char *get_next_file();
 void free_playlist();
