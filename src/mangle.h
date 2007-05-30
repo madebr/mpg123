b9235fad7bffde479c34b8276383d82a4d28a98a
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2007-05-30 17:52:04 +0000

    Trying to make assembler stuff work on MacOSX: 	- test for behaviour of .align (exponent or direct byte value) and macros in mangle.h 	- replace every align/p2align with ALIGN4, ALIGN8, ALIGN16 or ALIGN32 	- more BSS
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@664 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/mangle.h
+++ src/mangle.h
@@ -4,10 +4,25 @@
  * File licensed under the GPL, see http://www.fsf.org/ for more info.
  */
 
-/* ThOr: added the plain ASM_NAME */
+/* ThOr: added the plain ASM_NAME
+   Also this is getting more generic with the align stuff. */
 
 #ifndef __MANGLE_H
 #define __MANGLE_H
+
+#include "config.h"
+
+#ifdef ASMALIGN_EXP
+#define ALIGN4  .align 2
+#define ALIGN8  .align 3
+#define ALIGN16 .align 4
+#define ALIGN32 .align 5
+#else
+#define ALIGN4  .align 4
+#define ALIGN8  .align 8
+#define ALIGN16 .align 16
+#define ALIGN32 .align 32
+#endif
 
 /* Feel free to add more to the list, eg. a.out IMO */
 #if defined(__CYGWIN__) || defined(__MINGW32__) || defined(__OS2__) || \
