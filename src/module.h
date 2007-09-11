557d1d3dbbd5732c1a57900164c1cad1bd8f99bc
njh <njh@35dc7657-300d-0410-a2e5-dc2837fedb53>
2007-09-11 14:25:47 +0000

    Changed all occurrences of http://mpg123.de to http://mpg123.org (for consistency)
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@939 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/module.h
+++ src/module.h
@@ -2,7 +2,7 @@
 	module: module loading and listing interface
 
 	copyright ?-2007 by the mpg123 project - free software under the terms of the LGPL 2.1
-	see COPYING and AUTHORS files in distribution or http://mpg123.de
+	see COPYING and AUTHORS files in distribution or http://mpg123.org
 */
 
 #include "config.h"
