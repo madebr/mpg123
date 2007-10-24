0f3b32a95139c5edcaf4d4690f652bb3454270c8
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2007-10-24 15:33:02 +0000

    Only overwrite config.h when it changed. Makes it possible not to always rebuild everything.
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@1042 35dc7657-300d-0410-a2e5-dc2837fedb53

--- MakeLegacy.sh
+++ MakeLegacy.sh
@@ -20,13 +20,18 @@ cd src
 
 # Write out our own very basic config.h
 echo "Creating basic config.h to reproduce pre-autoconf days."
-cp config.h.legacy config.h &&
+cp config.h.legacy config.h.new &&
 {
 	echo "/* added by MakeLegacy.sh */"
 	echo "#define PACKAGE_NAME \"$PACKAGE_NAME\"" 
 	echo "#define PACKAGE_VERSION \"$PACKAGE_VERSION\""
 	echo "#define PACKAGE_BUGREPORT \"$PACKAGE_BUGREPORT\""
-} >> config.h &&
-
+} >> config.h.new &&
+if test "`diff -q config.h config.h.new`" = ""; then
+	echo "no change in config.h"
+else
+	echo "config.h changed... moving"
+	mv config.h.new config.h
+fi &&
 exec make -f Makefile.legacy $* ||
 echo "some error!?"
