d699d1b0f6f9b7e77474615e108f3f2df125c8e8
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2007-11-19 09:04:32 +0000

    Now computing the RMS with normalization of the difference to the input amplitude range. The former way of just normalizing to 32768 wasn't that bright an idea, I think.
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@1115 35dc7657-300d-0410-a2e5-dc2837fedb53

--- test/rms16.c
+++ test/rms16.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <limits.h>
 
 #define BS 16384
 
@@ -14,7 +15,10 @@ int main(int argc, char** argv)
 	int a = open(argv[1], O_RDONLY);
 	int b = open(argv[2], O_RDONLY);
 	if(a < 0 || b < 0){ fprintf(stderr, "cannot open files\n");return 1; }
-	short max = 0;
+	long max = 0;
+	/* amplitude spread for normalization */
+	short maxin = SHRT_MIN;
+	short minin = SHRT_MAX;
 	double rms = 0;
 	long count = 0;
 	long diffcount = 0;
@@ -28,17 +32,19 @@ int main(int argc, char** argv)
 		{
 		short av = buf1[i];
 		short bv = buf2[i];
-		short diff = av-bv;
+		long diff = (long)av-(long)bv;
+		if(av < minin) minin = av;
+		if(av > maxin) maxin = av;
 		if(diff < 0) diff *= -1;
 		if(diff > max) max = diff;
 		if(diff) ++diffcount;
 		/* if(diff > 10) fprintf(stderr, "diff %i at %li\n", diff, count); */
 		++count;
-		rms += pow((double)av/32768-(double)bv/32768,2);
+		rms += pow(diff,2);
 		}
 	}
-	rms /= count;
-	printf("RMS=%g; max=%i; diffcout=%li in count=%li\n", sqrt(rms), max, diffcount, count);
+	rms /= count*pow((long)maxin-(long)minin, 2);
+	printf("RMS=%g; max=%i; diffcount=%li in count=%li; input amplitude range %li, from %i to %i\n", sqrt(rms), max, diffcount, count, (long)maxin-(long)minin, minin, maxin);
 	close(a);
 	close(b);
 	return 0;
