7de3352d16386eebea6fb9c3927578a0a1a91b1c
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2007-11-28 08:41:53 +0000

    Add a note about error checking, fix description and include stdio.h .
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@1145 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/scan.c
+++ src/scan.c
@@ -1,11 +1,16 @@
 /*
-	scan: Esitmate length of a mpeg file and compare to length from exact scan.
+	scan: Estimate length (sample count) of a mpeg file and compare to length from exact scan.
 
 	copyright 2007 by the mpg123 project - free software under the terms of the LGPL 2.1
 	see COPYING and AUTHORS files in distribution or http://mpg123.org
 	initially written by Thomas Orgis
 */
 
+/* Note the lack of error checking here.
+   While it would be nicer to inform the user about troubles, libmpg123 is designed _not_ to bite you on operations with invalid handles , etc.
+  You just jet invalid results on invalid operations... */
+
+#include <stdio.h>
 #include "mpg123.h"
 
 int main(int argc, char **argv)
