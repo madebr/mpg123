a870c118ef33dcec386edce66f9fb29f56e90c97
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2007-12-28 18:18:51 +0000

    Cleaning up the XMMS2 plugin a bit... preparing for submitting it to them.
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@1323 35dc7657-300d-0410-a2e5-dc2837fedb53

--- xmms2-plugin/mpg123/mpg123.c
+++ xmms2-plugin/mpg123/mpg123.c
@@ -2,18 +2,23 @@
  *  Copyright (C) 2007 Thomas Orgis <thomas@orgis.org>
  *
  *  This is free software under the terms of the LGPL 2.1 .
+ *  For libmpg23 API have a look at http://mpg123.org/api/ .
  *
  *  This is also a very basic mpg123 decoder plugin.
  *  Configurable goodies of libmpg123 are missing:
  *   - user decoder choice
- *   - equalizer (_fast!_)
- *   - forced mono, resampling
+ *   - equalizer (fast, using the MPEG frequency bands directly)
+ *   - forced mono, resampling (xmms2 can handle that itself, I guess)
+ *   - choose RVA preamp (album/mix, from lame tag ReplayGain info or ID3v2 tags)
+ *  This should be easy to add for an XMMS2 hacker.
+ *
  *  Some basic metadata is read and set in the strem properties.
  *  With libmpg123 you can get at least all text and comment ID3v2 (version 2.2, 2.3, 2.4)
  *  frames as well as the usual id3v1 info (when you get to the end of the file...).
  *  The decoder also likes to read ID3 tags for getting RVA-related info that players like
  *  foobar2000 store there... Now the problem is: Usually, the id3 xform reads and cuts the id3 data,
  *  Killing the info for mpg123...
+ *  Perhaps one can make the generic id3 plugin store the necessary info for retrieval here, or just keep the id3 tags there...
  */
 
 #include "xmms/xmms_xformplugin.h"
@@ -58,10 +63,8 @@ static gboolean xmms_mpg123_plugin_setup(xmms_xform_plugin_t *xfp)
 	xmms_xform_methods_t methods;
 	int result;
 
-	XMMS_DBG("setup1");
 	result = mpg123_init();
 	if(result != MPG123_OK) return FALSE;
-	XMMS_DBG("setup2");
 	XMMS_XFORM_METHODS_INIT (methods);
 	methods.init    = xmms_mpg123_init;
 	methods.destroy = xmms_mpg123_destroy;
@@ -69,25 +72,22 @@ static gboolean xmms_mpg123_plugin_setup(xmms_xform_plugin_t *xfp)
 	methods.seek    = xmms_mpg123_seek;
 	xmms_xform_plugin_methods_set(xfp, &methods);
 
-	XMMS_DBG("setup3");
 	xmms_xform_plugin_indata_add(xfp, XMMS_STREAM_TYPE_MIMETYPE,
 	                             "audio/mpeg", /* "audio/x-mpeg", */
 	                             NULL);
-	XMMS_DBG("setup3a");
 	/* Well, I usually only see mp3 and mp2 ... layer 1 files are quite rare. */
 	xmms_magic_extension_add("audio/mpeg", "*.mp3");
-	XMMS_DBG("setup3b");
 	xmms_magic_extension_add("audio/mpeg", "*.mp2");
-	XMMS_DBG("setup3c");
 	xmms_magic_extension_add("audio/mpeg", "*.mp1");
 
 	/* Not so sure about magic numbers for metadata-infested mpeg audio... */
-	XMMS_DBG("setup4");
 
 	return TRUE;
 }
 
-/* Update metadata... fail silently. */
+/* Update metadata... fail silently.
+   There is more information to get from libmpg123's ID3v2 reader.
+   Note: All text is in UTF-8 encoding, regardless of ID3v2 source encoding! */
 static void xmms_mpg123_metacheck(xmms_xform_t *xform)
 {
 	xmms_mpg123_data_t *data;
@@ -95,9 +95,8 @@ static void xmms_mpg123_metacheck(xmms_xform_t *xform)
 	if(xform == NULL) return;
 	data = xmms_xform_private_data_get(xform);
 	if(data == NULL) return;
-	XMMS_DBG("check for new metadata");
+
 	mc = mpg123_meta_check(data->decoder);
-	XMMS_DBG("returned 0x%x", mc);
 	if(mc & MPG123_NEW_ID3)
 	{
 		XMMS_DBG("got new ID3v2 data");
@@ -134,38 +133,38 @@ static gboolean xmms_mpg123_init(xmms_xform_t *xform)
 	size_t num_rates;
 	mpg123_rates(&rates, &num_rates);
 
-	XMMS_DBG("init");
 	g_return_val_if_fail(xform, FALSE);
 	data = g_new0(xmms_mpg123_data_t, 1);
 	xmms_xform_private_data_set(xform, data);
-	XMMS_DBG("init2");
 
 	data->indata = 0;
 	data->alive  = 1;
 	data->param = mpg123_new_pars(&result);
 	g_return_val_if_fail(data->param, FALSE);
-	XMMS_DBG("init3");
 
-	/* Create a quiet (stderr) decoder with auto choosen optimization. */
-	/* Stuff set here should be tunable via plugin config properties. */
+	/* Create a quiet (stderr) decoder with auto choosen optimization.
+	 * Stuff set here should be tunable via plugin config properties!
+	 * You can also change some things during playback...
+	 */
 	mpg123_par(data->param, MPG123_ADD_FLAGS, MPG123_QUIET, 0);
 	mpg123_par(data->param, MPG123_ADD_FLAGS, MPG123_GAPLESS, 0);
 	/* choose: MPG123_RVA_OFF, MPG123_RVA_MIX, MPG123_RVA_ALBUM */
 	mpg123_par(data->param, MPG123_RVA, MPG123_RVA_ALBUM, 0);
-	data->decoder = mpg123_parnew(NULL, NULL, &result);
+	/* You could choose a decoder from the list provided by
+	 * mpg123_supported_decoders() and give that as second parameter.
+	 */
+	data->decoder = mpg123_parnew(data->param, NULL, &result);
 	if(data->decoder == NULL) {
 		xmms_log_fatal("%s", mpg123_plain_strerror(result));
 		goto bad;
 	}
 
-	XMMS_DBG("init4");
 	/* Prepare for buffer input feeding. */
 	result = mpg123_open_feed(data->decoder);
 	if(result != MPG123_OK) {
 		goto mpg123_bad;
 	}
 
-	XMMS_DBG("init5");
 	/* Let's always decode to signed 16bit for a start.
 	   Any mpg123-supported sample rate is accepted. */
 	if(MPG123_OK != mpg123_format_none(data->decoder)) {
@@ -177,7 +176,6 @@ static gboolean xmms_mpg123_init(xmms_xform_t *xform)
 			goto mpg123_bad;
 		}
 	}
-	XMMS_DBG("init6");
 	/* ID3v1 data should be fetched here */
 	do {
 		/* Parse stream and get info. */
@@ -192,7 +190,7 @@ static gboolean xmms_mpg123_init(xmms_xform_t *xform)
 			                       (size_t) fill, NULL, 0, &fakegot);
 		}
 	} while(result == MPG123_NEED_MORE); /* Keep feeding... */
-	XMMS_DBG("init7");
+
 	if(result != MPG123_NEW_FORMAT) {
 		xmms_log_fatal("Unable to get the stream going (%s)!",
 		               result == MPG123_ERR ? mpg123_strerror(data->decoder)
@@ -203,7 +201,6 @@ static gboolean xmms_mpg123_init(xmms_xform_t *xform)
 	xmms_mpg123_metacheck(xform);
 	result = mpg123_getformat(data->decoder,
 	                          &data->rate, &data->channels, &data->encoding);
-	XMMS_DBG("init8");
 	if(result != MPG123_OK) {
 		goto mpg123_bad;
 	}
@@ -275,11 +272,10 @@ static gint xmms_mpg123_read(xmms_xform_t *xform, xmms_sample_t *buf,
 					XMMS_DBG("apparently this is the end");
 				}
 				mpg123_decode(data->decoder, data->buf,
-		                       0, (unsigned char*)buf+have_read,
-		                       need, &have_now);
+				              0, (unsigned char*)buf+have_read,
+				              need, &have_now);
 				break;
 			}
-			XMMS_DBG("%lu bytes input (total pos %lu)", (unsigned long) fill, (unsigned long)data->indata);
 		}
 		data->indata += fill;
 		result = mpg123_decode(data->decoder, data->buf,
@@ -300,8 +296,8 @@ static gint xmms_mpg123_read(xmms_xform_t *xform, xmms_sample_t *buf,
 		xmms_log_error("mpg123 error: %s", mpg123_strerror(data->decoder));
 		return -1;
 	}
-	XMMS_DBG("%lu bytes in total, %lu bytes out", (unsigned long)data->indata, (unsigned long)have_read);
-	return (gint)(have_read); /*/data->bps);*/
+
+	return (gint)(have_read);
 }
 
 static gint64 xmms_mpg123_seek(xmms_xform_t *xform, gint64 samples,
