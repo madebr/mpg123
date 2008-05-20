dddc36918773e3fe2d49ded1538728af317463fd
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2008-05-20 18:56:32 +0000

    add that msvc header
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@1491 35dc7657-300d-0410-a2e5-dc2837fedb53

---
+++ ports/mpg123_msvc.h
@@ -0,0 +1,20 @@
+/*
+	libmpg123: MPEG Audio Decoder library (version 20080419000000)
+
+	copyright 1995-2007 by the mpg123 project - free software under the terms of the LGPL 2.1
+	see COPYING and AUTHORS files in distribution or http://mpg123.org
+*/
+
+
+#ifndef MPG123_MSVC_H
+#define MPG123_MSVC_H
+
+/* Define types for Visual Studio */
+#ifdef _MSC_VER
+typedef long ssize_t;
+#endif
+
+
+#include "mpg123.h"
+
+#endif
