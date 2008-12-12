66dcffc1ea3e4f40c5a283eeb71cbe42ad2b724d
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2008-12-12 14:46:38 +0000

    note about s16 scaling
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@1774 35dc7657-300d-0410-a2e5-dc2837fedb53

--- test/s16_double.c
+++ test/s16_double.c
@@ -6,7 +6,7 @@ int main()
 	double d;
 	while( read(STDIN_FILENO, &s, sizeof(short)) == sizeof(short) )
 	{
-		d = (double)s / 32768.;
+		d = (double)s / 32768.; /* funny: 32767 makes mpg123's s16 compare better with reference */
 		write(STDOUT_FILENO, &d, sizeof(double));
 	}
 	return 0;
