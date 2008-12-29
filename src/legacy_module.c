b89d2e2a9621f6719af2f6bb34836f407d7263f0
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2008-12-29 22:32:14 +0000

    typos
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@1790 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/legacy_module.c
+++ src/legacy_module.c
@@ -34,8 +34,8 @@ open_module( const char* type, const char* name )
 	
 	/* Check the module compiled in is the module requested */
 	if (strcmp(name, mod->name)!=0) {
-		error1("Unable to open requesed module '%s'.", name);
-		error1("The only available staticly compiled module is '%s'.", mod->name);
+		error1("Unable to open requested module '%s'.", name);
+		error1("The only available statically compiled module is '%s'.", mod->name);
 		return NULL;
 	}
 	
