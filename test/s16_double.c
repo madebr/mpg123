109936defb30a9e8beeb1c0fa32ce860b535b688
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2009-04-05 16:32:54 +0000

    faster s16_double too
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@1914 35dc7657-300d-0410-a2e5-dc2837fedb53

--- test/s16_double.c
+++ test/s16_double.c
@@ -1,13 +1,19 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int main()
 {
-	short s;
-	double d;
-	while( read(STDIN_FILENO, &s, sizeof(short)) == sizeof(short) )
+	const size_t bufs = 1024;
+	size_t got;
+	short s[bufs];
+	double d[bufs];
+	while( (got = fread(s, sizeof(short), bufs, stdin)) )
 	{
-		d = (double)s / 32768.; /* funny: 32767 makes mpg123's s16 compare better with reference */
-		write(STDOUT_FILENO, &d, sizeof(double));
+		size_t fi;
+		for(fi=0; fi<got; ++fi)
+		d[fi] = (double) s[fi] / 32768.; /* funny: 32767 makes mpg123's s16 compare better with reference */
+
+		write(STDOUT_FILENO, d, sizeof(double)*got);
 	}
 	return 0;
 }
