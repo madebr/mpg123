8d823967fa7b0dd91f394cf2cffe49f371ecb5ea
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2009-04-05 11:01:59 +0000

    prevent multiple needless mpg123_init() work
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@1908 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/libmpg123/libmpg123.c
+++ src/libmpg123/libmpg123.c
@@ -98,11 +98,12 @@ static void frame_buffercheck(mpg123_handle *fr)
 }
 #endif
 
-
 int attribute_align_arg mpg123_init(void)
 {
 	ALIGNCHECKK
 	if((sizeof(short) != 2) || (sizeof(long) < 4)) return MPG123_BAD_TYPES;
+
+	if(initialized) return MPG123_OK; /* no need to initialize twice */
 
 #ifndef NO_LAYER12
 	init_layer12(); /* inits also shared tables with layer1 */
@@ -119,7 +120,6 @@ int attribute_align_arg mpg123_init(void)
 void attribute_align_arg mpg123_exit(void)
 {
 	/* nothing yet, but something later perhaps */
-	if(initialized) return;
 }
 
 /* create a new handle with specified decoder, decoder can be "", "auto" or NULL for auto-detection */
