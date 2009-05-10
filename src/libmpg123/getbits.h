9c34b4f4ae19a41c61ff0148e71b8551648e0657
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2009-05-10 21:20:29 +0000

    Importing a C version of getbits. That fixes layer1 decoding.
    I am not sure what exactly went wrong with the macro, but the C code makes mpg123 a compliant layer 1 decoder again.
    This should be no performance regression, at least it is none on my system. The C code is still in the header, ready to be inlined for the same result.
    Maybe I will move all getbits macros to static C functions...
    
    
    
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@2024 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/libmpg123/getbits.h
+++ src/libmpg123/getbits.h
@@ -1,16 +1,18 @@
 /*
 	getbits
 
-	copyright ?-2006 by the mpg123 project - free software under the terms of the LGPL 2.1
+	copyright ?-2009 by the mpg123 project - free software under the terms of the LGPL 2.1
 	see COPYING and AUTHORS files in distribution or http://mpg123.org
 	initially written by Michael Hipp
+
+	All code is in the header to suggest/force inlining of these small often-used functions.
+	This indeed has some impact on performance.
 */
 
 #ifndef _MPG123_GETBITS_H_
 #define _MPG123_GETBITS_H_
 
-/* that's the same file as getits.c but with defines to
-  force inlining */
+#include "mpg123lib_intern.h"
 
 #define backbits(fr,nob) ((void)( \
   fr->bitindex    -= nob, \
@@ -20,12 +22,61 @@
 #define getbitoffset(fr) ((-fr->bitindex)&0x7)
 #define getbyte(fr)      (*fr->wordpointer++)
 
-#define getbits(fr, nob) ( \
-  fr->ultmp = fr->wordpointer[0], fr->ultmp <<= 8, fr->ultmp |= fr->wordpointer[1], \
-  fr->ultmp <<= 8, fr->ultmp |= fr->wordpointer[2], fr->ultmp <<= fr->bitindex, \
-  fr->ultmp &= 0xffffff, fr->bitindex += nob, \
-  fr->ultmp >>= (24-nob), fr->wordpointer += (fr->bitindex>>3), \
-  fr->bitindex &= 7,fr->ultmp)
+/* There is something wrong with that macro... the function below works also for the layer1 test case. */
+#define macro_getbits(fr, nob) ( \
+  fr->ultmp = fr->wordpointer[0],\
+  fr->ultmp <<= 8, \
+  fr->ultmp |= fr->wordpointer[1], \
+  fr->ultmp <<= 8, \
+  fr->ultmp |= fr->wordpointer[2], \
+  fr->ultmp <<= fr->bitindex, \
+  fr->ultmp &= 0xffffff, \
+  fr->bitindex += nob, \
+  fr->ultmp >>= (24-nob), \
+  fr->wordpointer += (fr->bitindex>>3), \
+  fr->bitindex &= 7, \
+  fr->ultmp)
+
+static unsigned int getbits(mpg123_handle *fr, int number_of_bits)
+{
+  unsigned long rval;
+
+#ifdef DEBUG_GETBITS
+fprintf(stderr,"g%d",number_of_bits);
+#endif
+
+/*  This is actually slow: if(!number_of_bits)
+    return 0; */
+
+#if 0
+   check_buffer_range(number_of_bits+fr->bitindex);
+#endif
+
+  {
+    rval = fr->wordpointer[0];
+    rval <<= 8;
+    rval |= fr->wordpointer[1];
+    rval <<= 8;
+    rval |= fr->wordpointer[2];
+
+    rval <<= fr->bitindex;
+    rval &= 0xffffff;
+
+    fr->bitindex += number_of_bits;
+
+    rval >>= (24-number_of_bits);
+
+    fr->wordpointer += (fr->bitindex>>3);
+    fr->bitindex &= 7;
+  }
+
+#ifdef DEBUG_GETBITS
+fprintf(stderr,":%lx\n",rval);
+#endif
+
+  return rval;
+}
+
 
 #define skipbits(fr, nob) fr->ultmp = ( \
   fr->ultmp = fr->wordpointer[0], fr->ultmp <<= 8, fr->ultmp |= fr->wordpointer[1], \
