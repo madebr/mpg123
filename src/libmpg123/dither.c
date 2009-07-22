37095b44e7d684728bf8b566f3b3a4469370de12
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2009-07-22 22:07:03 +0000

    add boilerplate
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@2227 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/libmpg123/dither.c
+++ src/libmpg123/dither.c
@@ -1,3 +1,11 @@
+/*
+	dither: Generate noise for dithering / noise shaping.
+
+	copyright 2009 by the mpg123 project - free software under the terms of the LGPL 2.1
+	see COPYING and AUTHORS files in distribution or http://mpg123.org
+	initially written by Taihei Monma
+*/
+
 #include "config.h"
 #include "compat.h"
 #include "dither.h"
