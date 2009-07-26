c60e6db718e017911e88dec22b4e8e9466c54122
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2009-07-26 08:41:51 +0000

    Structure noise stuff a bit... add hooks to export the noise, with test program using that. This could become part of the mpg123 API...
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@2234 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/libmpg123/dither.h
+++ src/libmpg123/dither.h
@@ -10,6 +10,14 @@
 #define MPG123_DITHER_H
 
 #define DITHERSIZE 65536
+enum mpg123_noise_type
+{
+	 mpg123_white_noise = 0
+	,mpg123_tpdf_noise
+	,mpg123_highpass_tpdf_noise
+};
+
+void mpg123_noise(float* table, size_t count, enum mpg123_noise_type noisetype);
 void dither_table_init(float *dithertable);
 
 #endif
