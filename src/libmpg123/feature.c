2356f23f1984cdc92a9022c4f6fc8c577c05886b
jon_y <jon_y@35dc7657-300d-0410-a2e5-dc2837fedb53>
2009-09-13 15:27:42 +0000

    fix lots of typos
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@2350 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/libmpg123/feature.c
+++ src/libmpg123/feature.c
@@ -10,84 +10,84 @@ int mpg123_feature(const enum mpg123_feature_set key)
 		  return 1;
 #else
 		  return 0;
-#else /* WANT_WIN32_UNICODE */
+#endif /* WANT_WIN32_UNICODE */
 
 		case MPG123_FEATURE_OUTPUT_8BIT:
 #ifdef NO_8BIT
 		return 0;
 #else
 		return 1;
-#else /* mpg123_output_8bit */
+#endif /* mpg123_output_8bit */
 
 		case MPG123_FEATURE_OUTPUT_16BIT:
 #ifdef NO_16BIT
 		return 0;
 #else
 		return 1;
-#else /* mpg123_output_16bit */
+#endif /* mpg123_output_16bit */
 
 		case MPG123_FEATURE_OUTPUT_32BIT:
 #ifdef NO_32BIT
 		return 0;
 #else
 		return 1;
-#else /* mpg123_output_32bit */
+#endif /* mpg123_output_32bit */
 
 		case MPG123_FEATURE_PARSE_ID3V2:
 #ifdef NO_ID3V2
 		return 0;
 #else
 		return 1;
-#else /* NO_ID3V2 */
+#endif /* NO_ID3V2 */
 
 		case MPG123_FEATURE_DECODE_LAYER1:
 #ifdef NO_LAYER1
 		return 0;
 #else
 		return 1;
-#else /* NO_LAYER1 */
+#endif /* NO_LAYER1 */
 
 		case MPG123_FEATURE_DECODE_LAYER2:
 #ifdef NO_LAYER2
 		return 0;
 #else
 		return 1;
-#else /* NO_LAYER2 */
+#endif /* NO_LAYER2 */
 
 		case MPG123_FEATURE_DECODE_LAYER3:
 #ifdef NO_LAYER3
 		return 0;
 #else
 		return 1;
-#else /* NO_LAYER3 */
+#endif /* NO_LAYER3 */
 
 		case MPG123_FEATURE_DECODE_ACCURATE:
 #ifdef ACCURATE_ROUNDING
 		return 1;
 #else
 		return 0;
-#else
+#endif /* ACCURATE_ROUNDING */
 
 		case MPG123_FEATURE_DECODE_DOWNSAMPLE:
 #ifdef NO_DOWNSAMPLE
 		return 0;
 #else
 		return 1;
-#else /* NO_DOWNSAMPLE */
+#endif /* NO_DOWNSAMPLE */
 
 		case MPG123_FEATURE_DECODE_NTOM:
 #ifdef NO_NTOM
 		return 0;
 #else
 		return 1;
-#else /* NO_NTOM */
+#endif /* NO_NTOM */
 
 		case MPG123_FEATURE_PARSE_ICY:
 #ifdef NO_ICY
 		return 0;
 #else
 		return 1;
-#else /* NO_ICY */
+#endif /* NO_ICY */
 
 		default: return 0;
 	}
