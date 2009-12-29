166f877ba4e3b5f8d92acc2b38952a4d92576eff
patr3ck <patr3ck@35dc7657-300d-0410-a2e5-dc2837fedb53>
2009-12-29 16:00:41 +0000

    Don't try to decode partial mp3 frames at the end of a file
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@2539 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/libmpg123/readers.c
+++ src/libmpg123/readers.c
@@ -369,9 +369,7 @@ static int generic_read_frame_body(mpg123_handle *fr,unsigned char *buf, int siz
 	{
 		long ll = l;
 		if(ll <= 0) ll = 0;
-
-		/* This allows partial frames at the end... do we really want to pad and decode these?! */
-		memset(buf+ll,0,size-ll);
+		return READER_MORE;
 	}
 	return l;
 }
