563b4b786d5e16224932e8a26aa53db0a1286b52
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2010-02-27 19:11:55 +0000

    Typo in license header.
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@2605 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/libmpg123/layer3.c
+++ src/libmpg123/layer3.c
@@ -1,5 +1,5 @@
 /*
-	leyer3.c: the layer 3 decoder
+	layer3.c: the layer 3 decoder
 
 	copyright 1995-2008 by the mpg123 project - free software under the terms of the LGPL 2.1
 	see COPYING and AUTHORS files in distribution or http://mpg123.org
