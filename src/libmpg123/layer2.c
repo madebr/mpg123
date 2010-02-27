2c7027412049accc99241ba80917d7de0fc30d32
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2010-02-27 19:13:56 +0000

    Update copyright years on layerN.c
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@2606 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/libmpg123/layer2.c
+++ src/libmpg123/layer2.c
@@ -1,7 +1,7 @@
 /*
 	layer2.c: the layer 2 decoder, root of mpg123
 
-	copyright 1994-2008 by the mpg123 project - free software under the terms of the LGPL 2.1
+	copyright 1994-2009 by the mpg123 project - free software under the terms of the LGPL 2.1
 	see COPYING and AUTHORS files in distribution or http://mpg123.org
 	initially written by Michael Hipp
 
--- src/libmpg123/layer3.c
+++ src/libmpg123/layer3.c
@@ -1,7 +1,7 @@
 /*
 	layer3.c: the layer 3 decoder
 
-	copyright 1995-2008 by the mpg123 project - free software under the terms of the LGPL 2.1
+	copyright 1995-2009 by the mpg123 project - free software under the terms of the LGPL 2.1
 	see COPYING and AUTHORS files in distribution or http://mpg123.org
 	initially written by Michael Hipp
 
