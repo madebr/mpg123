124bfbdc153313991cc400d4593c209b6ce8fdd1
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2010-03-29 01:26:08 +0000

    Outsource aligncheck.
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@2639 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/libmpg123/libmpg123.c
+++ src/libmpg123/libmpg123.c
@@ -22,39 +22,7 @@
 
 static int initialized = 0;
 
-#define ALIGNCHECK(mh)
-#define ALIGNCHECKK
-/* On compilers that support data alignment but not the automatic stack realignment.
-   We check for properly aligned stack before risking a crash because of badly compiled
-   client program. */
-#if (defined CCALIGN) && (defined NEED_ALIGNCHECK) && ((defined DEBUG) || (defined CHECK_ALIGN))
-
-/* Common building block. */
-#define ALIGNMAINPART \
-	/* minimum size of 16 bytes, not all compilers would align a smaller piece of data */ \
-	double ALIGNED(16) altest[2]; \
-	debug2("testing alignment, with %lu %% 16 = %lu", \
-		(unsigned long)altest, (unsigned long)((size_t)altest % 16)); \
-	if((size_t)altest % 16 != 0)
-
-#undef ALIGNCHECK
-#define ALIGNCHECK(mh) \
-	ALIGNMAINPART \
-	{ \
-		error("Stack variable is not aligned! Your combination of compiler/library is dangerous!"); \
-		if(mh != NULL) mh->err = MPG123_BAD_ALIGN; \
-\
-		return MPG123_ERR; \
-	}
-#undef ALIGNCHECKK
-#define ALIGNCHECKK \
-	ALIGNMAINPART \
-	{ \
-		error("Stack variable is not aligned! Your combination of compiler/library is dangerous!"); \
-		return MPG123_BAD_ALIGN; \
-	}
-
-#endif
+#include "aligncheck.h"
 
 #ifdef GAPLESS
 /*
