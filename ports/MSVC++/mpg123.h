56a8c9045db92608bcb7cdcf7871748f891b1981
patr3ck <patr3ck@35dc7657-300d-0410-a2e5-dc2837fedb53>
2012-01-18 14:16:45 +0000

    Improve compatibility with VS2010 Patch provided by Charles Van Winkle, cvanwink at adobe dot com
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@2997 35dc7657-300d-0410-a2e5-dc2837fedb53

--- ports/MSVC++/mpg123.h
+++ ports/MSVC++/mpg123.h
@@ -12,8 +12,14 @@
 #include <sys/types.h>
 
 typedef long ssize_t;
+
+// Needed for Visual Studio versions before VS 2010.
+#if (_MSC_VER < 1600)
 typedef __int32 int32_t;
 typedef unsigned __int32 uint32_t;
+#else
+#include <stdint.h>
+#endif
 
 #define PRIiMAX "I64i"
 typedef __int64 intmax_t;
