b9e0e19be722a748646f1032f25d4b162fb43f2a
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2013-02-11 09:11:53 +0000

    Also accept application/x-scpls.
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@3268 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/httpget.c
+++ src/httpget.c
@@ -80,7 +80,7 @@ static const char* mime_file[] =
 	NULL
 };
 static const char* mime_m3u[] = { "audio/mpegurl", "audio/mpeg-url", "audio/x-mpegurl", NULL };
-static const char* mime_pls[]	= { "audio/x-scpls", "audio/scpls", "application/pls", NULL };
+static const char* mime_pls[]	= { "audio/x-scpls", "audio/scpls", "application/pls", "application/x-scpls", NULL };
 static const char** mimes[] = { mime_file, mime_m3u, mime_pls, NULL };
 
 int debunk_mime(const char* mime)
