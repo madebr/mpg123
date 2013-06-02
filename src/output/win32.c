b8916d8aff1d86d443448a14da2532c677955669
jon_y <jon_y@35dc7657-300d-0410-a2e5-dc2837fedb53>
2013-06-02 22:26:46 +0000

    Update contributors list
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@3381 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/output/win32.c
+++ src/output/win32.c
@@ -1,11 +1,12 @@
 /*
     win32: audio output for Windows 32bit
 
-    copyright ?-2007 by the mpg123 project - free software under the terms of the LGPL 2.1
+    copyright ?-2013 by the mpg123 project - free software under the terms of the LGPL 2.1
     see COPYING and AUTHORS files in distribution or http://mpg123.org
 
     initially written (as it seems) by Tony Million
     rewrite of basic functionality for callback-less and properly ringbuffered operation by ravenexp
+    Closing buffer playback fixed by dw <limegreensocks (*) yahoo dod com>
 */
 
 #include "mpg123app.h"
