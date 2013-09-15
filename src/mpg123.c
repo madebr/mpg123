0da68096ca78884eb10ca3574c8f694403a60559
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2013-09-15 13:22:41 +0000

    it is LGPL, really
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@3393 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/mpg123.c
+++ src/mpg123.c
@@ -1241,7 +1241,7 @@ static void print_title(FILE *o)
 {
 	fprintf(o, "High Performance MPEG 1.0/2.0/2.5 Audio Player for Layers 1, 2 and 3\n");
 	fprintf(o, "\tversion %s; written and copyright by Michael Hipp and others\n", PACKAGE_VERSION);
-	fprintf(o, "\tfree software (LGPL/GPL) without any warranty but with best wishes\n");
+	fprintf(o, "\tfree software (LGPL) without any warranty but with best wishes\n");
 }
 
 static void usage(int err)  /* print syntax & exit */
