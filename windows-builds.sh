51c71562a39908477214365b342134dab3e7965c
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2014-05-10 10:36:27 +0000

    add out123.exe to windows builds
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@3555 35dc7657-300d-0410-a2e5-dc2837fedb53

--- windows-builds.sh
+++ windows-builds.sh
@@ -103,6 +103,7 @@ mpg123_build()
 	rm -rf "$final/$name" &&
 	mkdir  "$final/$name" &&
 	cp -v "$tmp/bin/mpg123.exe" "$final/$name" &&
+	cp -v "$tmp/bin/out123.exe" "$final/$name" &&
 	if test "$debug" = y; then
 		echo "Not stripping the debug build..."
 	else
