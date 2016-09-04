f7572acecaafa668068b4e91649337465f3db5ff
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2016-09-04 23:14:13 +0000

    Do not repeat genre for the simple cases of number and name.
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@4085 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/metaprint.c
+++ src/metaprint.c
@@ -283,7 +283,12 @@ void print_id3_tag(mpg123_handle *mh, int long_id3, FILE *out)
 				}
 				if(state == outtahere) break;
 			}
-			if(nonum < tmp.fill-1)
+			/* Small hack: Avoid repeating genre in case of stuff like
+			   (144)Thrash Metal being given. The simple cases. */
+			if(
+				nonum < tmp.fill-1 &&
+				(!tag[GENRE].fill || strncmp(tag[GENRE].p, tmp.p+nonum, tag[GENRE].fill))
+			)
 			{
 				if(tag[GENRE].fill) mpg123_add_string(&tag[GENRE], ", ");
 				mpg123_add_string(&tag[GENRE], tmp.p+nonum);
