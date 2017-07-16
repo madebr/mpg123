23cd576b4690fb8316bac165e20968538074a2c9
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2017-07-16 17:22:27 +0000

    out123: Fix the output of --test-encodings to be meaningful.
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@4288 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/out123.c
+++ src/out123.c
@@ -360,9 +360,18 @@ static void test_format(char *arg)
 
 static void test_encodings(char *arg)
 {
-	int encs;
+	int encs, enc_count, *enc_codes, i, j;
+
 	encs = getencs();
-	printf("%i\n", encs);
+	j=0;
+	enc_count = out123_enc_list(&enc_codes);
+	for(i=0;i<enc_count;++i)
+	{
+		if(encs & enc_codes[i] == enc_codes[i])
+			printf("%s%s", j++ ? " " : "", out123_enc_name(enc_codes[i]));
+	}
+	printf("\n");
+	free(enc_codes);
 	exit(!encs);
 }
 
