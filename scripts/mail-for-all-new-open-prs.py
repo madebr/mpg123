0142c5619fa65c988b9113b9cae78259f52b24d9
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-12-11 01:03:24 +0100

    Check for open prs every hour

---
+++ scripts/mail-for-all-new-open-prs.py
@@ -0,0 +1,78 @@
+import logging
+import os
+import smtplib
+import ssl
+
+import github
+import markdown
+
+logging.basicConfig(format='[%(asctime)s] %(message)s')
+logging.root.setLevel(logging.INFO)
+
+mailinglist_message = "The [mpg123-devel mailing list](https://sourceforge.net/p/mpg123/mailman/mpg123-devel/) has been notified of the existence of this pr."
+
+g = github.Github(os.environ["PUSH_GITHUB_TOKEN"])
+user_me = g.get_user()
+repo = g.get_repo(os.environ["GITHUB_REPOSITORY"])
+
+for pr in repo.get_pulls(state="open"):
+    logging.info("Checking PR #%d", pr.id)
+    already_handled = False
+    for comment in pr.get_comments():
+        if comment.user == user_me and mailinglist_message in comment.body:
+            logging.info("PR#%d was already handled", pr.id)
+            already_handled = True
+    if already_handled:
+        continue
+
+    logging.info("PR #%d was NOT handled already", pr.id)
+
+    mail_markdown = f"""\
+A pull request by [{pr.user.login}]({pr.user.html_url}) was opened at {pr.created_at}.
+
+Please visit [{pr.html_url}]({pr.html_url}) to give feedback and review the code.
+
+---
+
+{markdown.markdown(pr.body)}
+
+---
+
+- url: {pr.html_url}
+- patch: {pr.html_url}.patch
+"""
+    logging.info("markdown message: %s", mail_markdown)
+
+    mail_title = "Opened GH-#{}: {} [{}]".format(pr.id, pr.title, pr.user.login)
+    mail_body = markdown.markdown(mail_markdown)
+
+    logging.info("mail title: %s", mail_title)
+    logging.info("mail body: %s", mail_body)
+
+    mail_message = f"""\
+MIME-Version: 1.0
+Content-type: text/html; charset=utf-8
+Subject: {mail_title}
+
+{mail_body}
+"""
+
+    sender_email = "{} <{}>".format("GitHub User {}".format(pr.user.login), os.environ["MAIL_SENDER"])
+    receiver_email = "contact list <{}>".format(os.environ["MAIL_RECEIVER"])
+
+    port = 465
+    server = os.environ["MAIL_SERVER"]
+    login = os.environ["MAIL_LOGIN"]
+    password = os.environ["MAIL_PASSWORD"]
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(server, port, context=context) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, mail_message)
+
+    logging.info("Creating comment at PR#%d", pr.id)
+    comment = pr.create_issue_comment(mailinglist_message)
+    logging.info("Visit comment at %s", comment.html_url)
+
+    logging.info("mail sent")
