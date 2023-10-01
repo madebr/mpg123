1933e759cade0222b32246370f0d92db0300810c
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2023-10-01 11:23:18 +0000

    libsyn123: re-introduce explicit _32 wrappers
    git-svn-id: svn://scm.orgis.org/mpg123/trunk@5367 35dc7657-300d-0410-a2e5-dc2837fedb53

--- src/libsyn123/resample.c
+++ src/libsyn123/resample.c
@@ -2043,6 +2043,8 @@ resample_total_alias(off_t, syn123_resample_intotal, syn123_resample_intotal64)
 #elif SIZEOF_OFF_T == 4
 resample_total_wrap(off_t, INT32_MAX, syn123_resample_total, syn123_resample_total64)
 resample_total_wrap(off_t, INT32_MAX, syn123_resample_intotal, syn123_resample_intotal64)
+resample_total_wrap(off_t, INT32_MAX, syn123_resample_total_32, syn123_resample_total64)
+resample_total_wrap(off_t, INT32_MAX, syn123_resample_intotal_32, syn123_resample_intotal64)
 #ifdef LFS_LARGEFILE_64
 resample_total_alias(off64_t, syn123_resample_total_64, syn123_resample_total64)
 resample_total_alias(off64_t, syn123_resample_intotal_64, syn123_resample_intotal64)
